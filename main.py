1513ad7cc6a8a6e689792c3f3c233f3b33258958
ArtemADD <151026785+ArtemADD@users.noreply.github.com>
2024-02-09 18:08:09 +0500

    3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
@@ -18,6 +18,23 @@ def promotion():
     words = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
              'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
     return '</br>'.join(words)
+
+
+@app.route('/image_mars')
+def return_mars_page():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='MARS.png')}" 
+                        alt="здесь должна была быть картинка, но не нашлась">
+                    <br>Вот она какая, красная планета.
+                  </body>
+                </html>"""
 
 
 if __name__ == '__main__':
