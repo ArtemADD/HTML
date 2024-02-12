0f07b6b997fa1ec133324d2ba596fa07521ef92c
ArtemAdd <artm.baranov.08@gmail.com>
2024-02-12 13:44:59 +0500

    6

--- main.py
+++ main.py
@@ -185,5 +185,37 @@ def astronaut_selection():
         return "Форма отправлена"
 
 
+@app.route('/choice/<planet>')
+def choice(planet):
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Варианты выбора</title>
+                  </head>
+                  <body>
+                    <h1>Моё предложение: {planet}</h1>
+                    <h2>Эта планета близка к земле;</h2>
+                    <div class="alert alert-success">
+                        <h2>На ней много необходимых ресурсов;
+                    </div>
+                    <div class="alert alert-secondary">
+                        <h2>На ней есть вода и атмосфера;
+                    </div>
+                    <div class="alert alert-warning">
+                        <h2>На ней есть небольшое магнитное поле;
+                    </div>
+                    <div class="alert alert-danger">
+                        <h2>Наконец, она просто красивая!
+                    </div>
+                  </body>
+                </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
