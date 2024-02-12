6e9d57ee694cb253645fc1b4af33c6af540ca11f
ArtemAdd <artm.baranov.08@gmail.com>
2024-02-12 12:47:08 +0500

    4

--- main.py
+++ main.py
@@ -30,9 +30,46 @@ def return_mars_page():
                   </head>
                   <body>
                     <h1>Жди нас, Марс!</h1>
-                    <img src="{url_for('static', filename='MARS.png')}" 
+                    <img src="{url_for('static', filename='img/MARS.png')}" 
                         alt="здесь должна была быть картинка, но не нашлась">
-                    <br>Вот она какая, красная планета.
+                    <h3>Вот она какая, красная планета.</h3>
+                  </body>
+                </html>"""
+
+
+@app.route('/promotion_image')
+def return_promotion_image():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}"/>
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/MARS.png')}" 
+                        alt="здесь должна была быть картинка, но не нашлась">
+                    <div class="alert alert-dark">
+                        <h3>Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-success">
+                        <h3>Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-secondary">
+                        <h3>Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning">
+                        <h3>И начнем с Марса!
+                    </div>
+                    <div class="alert alert-danger">
+                        <h3>Присоединяйся!
+                    </div>
                   </body>
                 </html>"""
 
