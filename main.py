7d88a0c9de1eb871b8e1cad23189e5dc72392038
ArtemAdd <artm.baranov.08@gmail.com>
2024-02-13 18:10:52 +0500

    2шаблоны

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def index():
     return render_template('base.html', title='Заголовок')
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('index.html', title=prof, prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
