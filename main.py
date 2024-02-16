08ab9edab54c67f61e61dbe17640634f6a50a761
ArtemAdd <artm.baranov.08@gmail.com>
2024-02-16 17:22:38 +0500

    4шаблоны

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import *
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
@@ -17,6 +21,21 @@ def training(prof):
 @app.route('/list_prof/<list>')
 def list_prof(list):
     return render_template('3.html', list=list)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    param = {}
+    param['title'] = 'Анкета'
+    param['surname'] = 'Watny'
+    param['name'] = 'Mark'
+    param['education'] = 'выше среднего'
+    param['profession'] = 'штурман марсохода'
+    param['sex'] = 'male'
+    param['motivation'] = 'Всегда мечтал застрять на марсе!'
+    param['ready'] = 'True'
+    return render_template('auto_answer.html', **param)
 
 
 if __name__ == '__main__':
