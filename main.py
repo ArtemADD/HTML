a81e03ba5fbd64d851d6cd09ed56cbf1ee32b480
ArtemAdd <artm.baranov.08@gmail.com>
2024-03-01 18:01:49 +0500

    sql4

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from data import db_session
 from data.users import User
+from data.jobs import Jobs
 from forms.user import RegisterForm
 
 app = Flask(__name__)
@@ -10,46 +11,13 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def main():
     db_session.global_init("db/mars_explorer.db")
     db_sess = db_session.create_session()
-    user = User()
-    user.surname = 'Scott'
-    user.name = 'Ridley'
-    user.age = 21
-    user.position = 'captain'
-    user.speciality = 'research engineer'
-    user.address = 'module_1'
-    user.email = 'scott_chief@mars.org'
-    db_sess.add(user)
-
-    user = User()
-    user.surname = 'Scott'
-    user.name = 'Ridley'
-    user.age = 21
-    user.position = 'captain'
-    user.speciality = 'research engineer'
-    user.address = 'module_1'
-    user.email = 'scott_chief1@mars.org'
-    db_sess.add(user)
-
-    user = User()
-    user.surname = 'Scott'
-    user.name = 'Ridley'
-    user.age = 21
-    user.position = 'captain'
-    user.speciality = 'research engineer'
-    user.address = 'module_1'
-    user.email = 'scott_chief2@mars.org'
-    db_sess.add(user)
-
-    user = User()
-    user.surname = 'Scott'
-    user.name = 'Ridley'
-    user.age = 21
-    user.position = 'captain'
-    user.speciality = 'research engineer'
-    user.address = 'module_1'
-    user.email = 'scott_chief3@mars.org'
-    db_sess.add(user)
-
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.is_finished = False
+    db_sess.add(job)
     db_sess.commit()
     app.run()
 
